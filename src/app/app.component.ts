a65f74103097a823d3ed66f2357968c0c753c227
Alberto Marin <alberto.marind@gmail.com>
2023-11-01 19:39:19 -0600

    Se crea pantalla de registro en linea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'sivimss-externo-ui';
+export class AppComponent implements OnInit, AfterViewInit {
+  constructor(
+    private primengConfig: PrimeNGConfig,
+    private translateService: TranslateService
+  ) {}
+
+  ngOnInit(): void {
+    this.permitirAnimacionRippleComponentesPrime();
+    this.establecerIdiomaGeneral('es');
+  }
+
+  ngAfterViewInit(): void {
+    this.establecerIdiomaComponentesPrime('es');
+  }
+
+  establecerIdiomaGeneral(idiomaGeneral: string) {
+    this.translateService.setDefaultLang(idiomaGeneral);
+  }
+
+  permitirAnimacionRippleComponentesPrime() {
+    this.primengConfig.ripple = true;
+  }
+
+  establecerIdiomaComponentesPrime(idioma: string) {
+    this.translateService.use(idioma);
+    this.translateService
+      .get('primeng')
+      .subscribe((res) => this.primengConfig.setTranslation(res));
+  }
 }
--- src/app/pages/publico/pages/registro/registro.module.ts
+++ src/app/pages/publico/pages/registro/registro.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { RegistroRoutingModule } from './registro-routing.module';
 import { RegistroComponent } from './registro.component';
 import { TituloPrincipalModule } from 'src/app/shared/titulo-principal/titulo-principal.module';
-
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    RegistroComponent
-  ],
+  declarations: [RegistroComponent],
   imports: [
     CommonModule,
     RegistroRoutingModule,
-    TituloPrincipalModule
-  ]
+    FormsModule,
+    ReactiveFormsModule,
+    TituloPrincipalModule,
+    CalendarModule,
+    DropdownModule,
+  ],
 })
-export class RegistroModule { }
+export class RegistroModule {}
